0000a629d090fed567966042281e39713e048966
AndreiCocan <95496161+AndreiCocan@users.noreply.github.com>
2022-02-21 20:13:15 +0100

    Direction linked to user
    #Features
    
    On init, a waypoint is attributed to the user's location

--- Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -18,6 +18,9 @@
 		Vector2d[] _locations;
 
 		[SerializeField]
+		GameObject _user;
+
+		[SerializeField]
 		float _spawnScale = 100f;
 
 		[SerializeField]
@@ -29,6 +32,11 @@
 		{
 			_locations = new Vector2d[_locationStrings.Length];
 			_spawnedObjects = new List<GameObject>();
+			var user_instance = Instantiate(_markerPrefab, transform);
+			//user_instance.transform.localPosition = _user.transform.position;
+			user_instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+			
+
 			for (int i = 0; i < _locationStrings.Length; i++)
 			{
 				var locationString = _locationStrings[i];
@@ -38,14 +46,24 @@
 				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
 				_spawnedObjects.Add(instance);
 			}
+			_spawnedObjects.Add(user_instance);
 		}
 
 		private void Update()
 		{
+			
+			Vector3 user_pos= _user.transform.position;
+			Debug.Log(user_pos);
 			int count = _spawnedObjects.Count;
-			for (int i = 0; i < count; i++)
+			var spawnedObject = _spawnedObjects[count - 1];
+			spawnedObject.transform.position = user_pos;
+			Debug.Log(spawnedObject.transform.position);
+			spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+
+
+			for (int i = 0; i < count-1; i++)
 			{
-				var spawnedObject = _spawnedObjects[i];
+				spawnedObject = _spawnedObjects[i];
 				var location = _locations[i];
 				spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
 				spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);

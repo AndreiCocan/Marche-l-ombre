6e5b5d8fe4970d07ceae4ca85ddee135b34be79a
LeViquoi <victor.robert-andrieu@insa-rennes.fr>
2022-05-04 13:39:17 +0200

    Button containing datas on the interface
    +create button each time new data is loaded
    +give this button an on click event to show the information
    +new button prefab

--- Assets/Scripts/Interface/Info_Interface.cs
+++ Assets/Scripts/Interface/Info_Interface.cs
@@ -7,26 +7,43 @@ using System;
 
 public class Info_Interface : MonoBehaviour
 {
-    /* TODO */
-
     // Variables for the vision of the camera
-    [SerializeField] GameObject camera;
+    private Camera _camera;
     private Vector3 direction;
     private Vector3 coordinate;
     private Vector3 difference;
     private float vu;
     private double distance;
     
-    //Variables for the text zones on the interface
-    [SerializeField] TextMeshProUGUI namePoint;
-    [SerializeField] TextMeshProUGUI geographicInfos;
-    [SerializeField] TextMeshProUGUI detailledInfos;
+    //Variables for the interface
+    private TextMeshProUGUI namePoint;
+    private TextMeshProUGUI geographicInfos;
+    private TextMeshProUGUI detailledInfos;
+    public Button info_button;
+    public Canvas choice_buttons;
+    public Canvas info_interface;
+    private Vector2 cornerTopRight;
+    private Vector2 cornerBottomLeft;
+
+    private static Dictionary<string, pages> pages;
+
+    private void Start()
+    {
+        pages = new Dictionary<string, pages>();
+        _camera = FindObjectOfType<Camera>();
+        namePoint = (TextMeshProUGUI)FindObjectsOfType<TextMeshProUGUI>().GetValue(0);
+        geographicInfos = (TextMeshProUGUI)FindObjectsOfType<TextMeshProUGUI>().GetValue(1);
+        detailledInfos = (TextMeshProUGUI)FindObjectsOfType<TextMeshProUGUI>().GetValue(2);
+
+        cornerTopRight = new Vector2(-265, 94);
+        cornerBottomLeft = new Vector2(265, -97);
+    }
 
     // Detect if the user is looking at the point of interest
     private bool IsFacing()
     {
         coordinate = /* Take the exact coordinate of the point of interest (API script) */
-        direction = (coordinate - camera.transform.position).normalized;
+        direction = (coordinate - _camera.transform.position).normalized;
         vu = Vector3.Dot(direction, transform.forward);
 
         if (vu <= 1 && vu >= 0.7)
@@ -42,7 +59,7 @@ public class Info_Interface : MonoBehaviour
     private bool IsNear()
     {
         coordinate = /* Take the exact coordinate of the point of interest (API script) */
-        difference = new Vector3(camera.transform.position.x - coordinate.x, camera.transform.position.y - coordinate.y, camera.transform.position.z - coordinate.z);
+        difference = new Vector3(_camera.transform.position.x - coordinate.x, _camera.transform.position.y - coordinate.y, _camera.transform.position.z - coordinate.z);
         distance = Math.Sqrt(Math.Pow(difference.x, 2f) + Math.Pow(difference.y, 2f) + Math.Pow(difference.z, 2f));
         if (distance < 40)
         {
@@ -54,9 +71,40 @@ public class Info_Interface : MonoBehaviour
     }
 
     // Change the informations on the canva depending on the point of interest location
-    void UpdateInfos()
+    public int UpdateInfos(Data data)
     {
-        /* Select the right infos on the json file */
+        foreach (pages page in data.pages)
+        {
+            if(!pages.ContainsKey(page.pageid)) pages.Add(page.pageid, page);
+        }
+
+        foreach (string id in pages.Keys)
+        {
+            Button b = CreateButton(info_button, choice_buttons, cornerTopRight, cornerBottomLeft);
+            b.GetComponent<TextMeshProUGUI>().text = pages[id].title;
+            b.onClick.AddListener(delegate { ShowInfosOnClick(id); });
+        }
+        return 0;
+    }
+
+    public void ShowInfosOnClick(string id)
+    {
+        namePoint.text = pages[id].title;
+        geographicInfos.text = "latitude : " + pages[id].lat + " | longitude : " + pages[id].lon;
+        detailledInfos.text = pages[id].extract;
+        info_interface.gameObject.SetActive(true);
+    }
+
+    private static Button CreateButton(Button buttonPrefab, Canvas canvas, Vector2 cornerTopRight, Vector2 cornerBottomLeft)
+    {
+        var button = Instantiate(buttonPrefab, Vector3.zero, Quaternion.identity);
+        var rectTransform = button.GetComponent<RectTransform>();
+        rectTransform.SetParent(canvas.transform);
+        rectTransform.anchorMax = cornerTopRight;
+        rectTransform.anchorMin = cornerBottomLeft;
+        rectTransform.offsetMax = Vector2.zero;
+        rectTransform.offsetMin = Vector2.zero;
+        return button;
     }
 
     // Update is called once per frame
@@ -64,7 +112,7 @@ public class Info_Interface : MonoBehaviour
     {
         if (IsFacing() && IsNear())
         {
-            UpdateInfos();
+           // UpdateInfos();
         }
     }
 }
